b0f0ad01594e4c75648cdd5c5bd262381aecced2
medineekanat7474 <145452232+medineekanat7474@users.noreply.github.com>
2023-10-10 16:00:30 +0300

    Add files via upload

---
+++ kasauygulama/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace kasauygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.btnYatir = new System.Windows.Forms.Button();
+            this.btnCek = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblBakiye = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TUTAR";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(85, 35);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(177, 20);
+            this.txtTutar.TabIndex = 1;
+            // 
+            // btnYatir
+            // 
+            this.btnYatir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYatir.Location = new System.Drawing.Point(85, 76);
+            this.btnYatir.Name = "btnYatir";
+            this.btnYatir.Size = new System.Drawing.Size(81, 41);
+            this.btnYatir.TabIndex = 2;
+            this.btnYatir.Text = "YATIR";
+            this.btnYatir.UseVisualStyleBackColor = true;
+            this.btnYatir.Click += new System.EventHandler(this.btnYatir_Click);
+            // 
+            // btnCek
+            // 
+            this.btnCek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCek.Location = new System.Drawing.Point(185, 76);
+            this.btnCek.Name = "btnCek";
+            this.btnCek.Size = new System.Drawing.Size(77, 41);
+            this.btnCek.TabIndex = 3;
+            this.btnCek.Text = "ÇEK";
+            this.btnCek.UseVisualStyleBackColor = true;
+            this.btnCek.Click += new System.EventHandler(this.btnCek_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblBakiye);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(314, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 102);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "HESAP";
+            // 
+            // lblBakiye
+            // 
+            this.lblBakiye.AutoSize = true;
+            this.lblBakiye.Location = new System.Drawing.Point(21, 41);
+            this.lblBakiye.Name = "lblBakiye";
+            this.lblBakiye.Size = new System.Drawing.Size(79, 20);
+            this.lblBakiye.TabIndex = 0;
+            this.lblBakiye.Text = "BAKİYE:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 209);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCek);
+            this.Controls.Add(this.btnYatir);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.Button btnYatir;
+        private System.Windows.Forms.Button btnCek;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblBakiye;
+    }
+}
+
---
+++ kasauygulama/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kasauygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double bakiye;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bakiye = 0;
+            lblBakiye.Text = "bakiye: " + bakiye.ToString() + "₺";
+        }
+
+        private void btnYatir_Click(object sender, EventArgs e)
+        {
+            double yatirilacaktutar=Convert.ToDouble(txtTutar.Text);
+            bakiye += yatirilacaktutar;
+            lblBakiye.Text = "bakiye: " + bakiye.ToString() + "₺";
+        }
+
+        private void btnCek_Click(object sender, EventArgs e)
+        {
+            double cekilecektutar = Convert.ToDouble(txtTutar.Text);
+            if (cekilecektutar <= bakiye)
+            {
+                bakiye -= cekilecektutar;
+            }
+            else
+            {
+                MessageBox.Show("YETERSİZ BAKİYE");
+            }
+
+
+            lblBakiye.Text = "bakiye: " + bakiye.ToString() + "₺";
+        }
+    }
+}
